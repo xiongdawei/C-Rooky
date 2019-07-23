79f26e7bc000ec37fc18c6c9bc8ea120368e3f4d
Mr.Нет <42790040+xiongdawei@users.noreply.github.com>
2019-07-23 14:24:27 +0800

    Add files via upload

---
+++ July22HW/ex4.25.cpp
@@ -0,0 +1,56 @@
+//
+// Created by Mr.нет's   on 2019-07-23.
+//
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+//a
+std::string problem_a(int x = 5, int y = 8){
+    if (x==5 and y==8){
+        cout<< "@@@@@"<<endl;
+    }
+    else{
+        cout<< "#####"<<endl;
+    }
+    cout<< "$$$$$" << endl;
+    cout<< "&&&&&" << endl;
+}
+
+std::string problem_b(int x = 5, int y = 8){
+    if (x==5 and y==8){
+        cout<< "@@@@@"<<endl;
+    }
+    else{
+        cout<< "#####"<<endl;
+        cout<< "$$$$$" << endl;
+        cout<< "&&&&&" << endl;
+    }
+
+}
+
+std::string problem_c(int x = 5, int y = 8){
+    if (x==5 and y==8){
+        cout<< "@@@@@"<<endl;
+    }
+    else{
+        cout<< "#####"<<endl;
+        cout<< "$$$$$" << endl;
+
+    }
+    cout<< "&&&&&" << endl;
+}
+
+std::string problem_d(int x = 5, int y = 8){
+    if (x==5 and y==7){
+        cout<< "@@@@@"<<endl;
+    }
+    else{
+        cout<< "#####"<<endl;
+        cout<< "$$$$$" << endl;
+        cout<< "&&&&&" << endl;
+    }
+
+}
