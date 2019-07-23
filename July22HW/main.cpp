c6bf1d69da165617e48532c46006f88c61f28b28
Mr.Нет <42790040+xiongdawei@users.noreply.github.com>
2019-07-23 14:04:09 +0800

    Add files via upload

---
+++ July22HW/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include "rational.cpp"
+#include "test005.cpp"
+
+// Angle brace is looking for the file
+// the quotation is looking for the class
+// #include"Account.h" the name is the relative path
+// "include/Account.h"
+
+
+
+
+
+// Phase 1 consists of editing a file with an editor program, normally known simply as an editor
+// Type c extension, which indicate that a file contains c++ source code
+// typical c++ development environment -- editing phase
+
+/*
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+int main(){
+    vector<int>obj;
+    for(int i=0;i<10;i++)
+    {
+        obj.push_back(i);
+        cout<<obj[0]<<',';
+    }
+    for(int i=0;i<5;i++)
+    {
+        obj.pop_back();
+    }
+    cout<<"\n"<<endl;
+
+    for(int i=0;i<obj.size();i++)
+    {
+        cout<<obj[i]<<",";
+    }
+    return 0;
+}
+
+
+
+
+
+double mean(double,double,double);
+int compare(int, int);
+int numm(int);
+
+class Account{
+public:
+    explicit Account(std::string accountName)
+            :name{accountName}{}
+
+    void setName(std::string accountName){
+        name = accountName;
+    }
+
+    std::string getName()const{
+        return name;
+    }
+private:
+    std::string name;
+
+};
+
+int main() {
+
+    Number num1;
+
+
+
+
+    std::cout <<num1.getNumr() << std::endl;
+    Student david;
+    std::string fn = "Dawei";
+    std::string ln = "Xiong";
+    int grade = 89;
+    std::string name = "C++";
+    david.add_data(fn,ln,name,grade);
+    std::cout << david.getName() << std::endl;
+
+
+
+
+    return 0;
+}
+
+*/
