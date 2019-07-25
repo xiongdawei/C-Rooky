da9297205a2e822324c23605613193077397fbf9
Mr.Нет <42790040+xiongdawei@users.noreply.github.com>
2019-07-25 18:14:51 +0800

    Add files via upload

---
+++ Notes_July25th/array002.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Mr.нет's   on 2019-07-25.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <array>
+using namespace std;
+/*
+int main(){
+    const size_t arraySize{5}; // must initialize in declaration
+    array<int, arraySize> values;
+    for (size_t i{0}; i<values.size();++i){
+        values[i] = 2 + 2*i;
+    }
+    for (size_t j{0};j< values.size();++j){
+        std::cout << setw(7) << j << setw(10) << values[j] << endl;
+    }
+
+}*/
+// C++ provides no automatic array bounds checking to prevent you from referring to an element with it,
+// calss templates array and vector each have an at function that perform
+
